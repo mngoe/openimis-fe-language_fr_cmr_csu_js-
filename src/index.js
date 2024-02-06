5a7e86d55a320559dcfc081587438113b075f37d
jobil <j.bilongo@ynote.africa>
2024-02-06 14:58:22 +0100

    last changes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import messages_fr_cm_csu from "./translations/fr_CM.json";
+import messages_en_cm_csu from "./translations/en_CM.json";
 
 const DEFAULT_CONFIG = {
   "translations": [
-    { key: 'fr', messages: messages_fr_cm_csu }
+    { key: 'fr', messages: messages_fr_cm_csu },
+    { key: 'en', messages: messages_en_cm_csu }
   ],
 }
 
