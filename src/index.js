e09d13499ee031e8adedb3bb5ce7359328286134
jobil <j.bilongo@ynote.africa>
2024-02-06 12:38:10 +0100

    last update translation

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import messages_fr_cm_csu from "./translations/fr_CM.json";
+import messages_en_cm_csu from "./translations/en_CM.json";
 
 const DEFAULT_CONFIG = {
-  "translations": [{ key: 'fr', messages: messages_fr_cm_csu }],
+  "translations": [
+    { key: 'fr', messages: messages_fr_cm_csu },
+    { key: 'en', messages: messages_en_cm_csu }
+  ],
 }
 
 export const LanguageFrCmrCSUModule = (cfg) => {
